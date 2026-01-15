2b1d9f522f10ff33b992ec1ee80ba5e1482305d0
ammkazi <amm.kazi@gmail.com>
2026-01-15 21:05:24 +0530

    For Loop in java with example

---
+++ Practical11.java
@@ -0,0 +1,27 @@
+// For Loop in java with example
+// Print first 10 natural numbers using for loop
+        
+public class Practical11 {
+    public static void main(String[] args) {
+        // Print first 10 natural numbers using for loop
+        System.out.println("First 10 natural numbers:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+/*
+OUTPUT: 
+First 10 natural numbers:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+*/
