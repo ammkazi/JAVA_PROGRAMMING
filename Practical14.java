8fe7696a1133dd2cbb45a951b306bafb6911cbb2
ammkazi <amm.kazi@gmail.com>
2026-01-15 21:07:24 +0530

    Array in java - Single Dimensional Array

---
+++ Practical14.java
@@ -0,0 +1,23 @@
+// Array in java - Single Dimensional Array
+public class Practical14 {
+    public static void main(String[] args) {
+        // declare and initialize an array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // print the elements of the array
+        System.out.println("Array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
+
+/*
+OUTPUT:
+Array elements:
+10
+20
+30
+40
+50
+*/
