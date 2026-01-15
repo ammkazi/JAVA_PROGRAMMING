39cb5c110fbf976e9a93416d86c2cb54568c23a6
ammkazi <amm.kazi@gmail.com>
2026-01-15 21:01:02 +0530

    while loop - print 1-10

---
+++ Practical8.java
@@ -0,0 +1,25 @@
+// looping statements in java
+// while loop
+public class Practical8 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
+/*
+OUTPUT:
+1
+2
+3
+4   
+5
+6
+7
+8
+9
+10
+*/
