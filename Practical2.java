bd34dd902888133d991b540116ea4900159b8ea9
ammkazi <amm.kazi@gmail.com>
2026-01-15 20:50:17 +0530

    Java tokens and data types, symbolic constant

---
+++ Practical2.java
@@ -0,0 +1,35 @@
+//  Java tokens and data types, symbolic constant
+public class Practical2 {
+    public static void main(String[] args) {
+
+        // Defining a symbolic constant
+        final double PI = 3.14159;
+
+        // Different data types
+        int intValue = 10;               // Integer data type
+        double doubleValue = 20.5;       // Double data type
+        char charValue = 'A';            // Character data type
+        boolean boolValue = true;        // Boolean data type
+        String stringValue = "Hello";    // String data type
+
+        // Displaying the values
+        System.out.println("Integer Value: " + intValue);
+        System.out.println("Double Value: " + doubleValue);
+        System.out.println("Character Value: " + charValue);
+        System.out.println("Boolean Value: " + boolValue);
+        System.out.println("String Value: " + stringValue);
+        System.out.println("Value of PI (Symbolic Constant): " + PI);
+    }
+}
+
+/*
+OUTPUT:
+
+Integer Value: 10
+Double Value: 20.5
+Character Value: A
+Boolean Value: true
+String Value: Hello
+Value of PI (Symbolic Constant): 3.14159
+
+*/
