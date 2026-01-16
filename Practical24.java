0bf834960f4ff48fa1980ed60528e17c5ead9854
ammkazi <amm.kazi@gmail.com>
2026-01-16 21:44:29 +0530

    command line arguments in java - Simple Example

---
+++ Practical24.java
@@ -0,0 +1,15 @@
+// command line arguments in java - Example 2
+public class Practical24 {
+    public static void main(String[] args) {
+        System.out.println("Args 1 " + args[0]);
+        System.out.println("Args 2 " + args[1]);
+    }
+}
+
+/*
+Output :
+Compile - javac Practical24.java
+Run - java Practical24 Hello World
+Args 1 Hello
+Args 2 World
+*/
